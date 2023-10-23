7053ec0d01a0cdfdb469c5406f5ffdc4340ab8a3
nFeus <barca105@nate.com>
2023-10-23 18:04:45 +0900

    ~ Chapter 2

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,12 +1,56 @@
 use std::str::FromStr;
 use num::Complex;
-use image::ColorType;
-use image::png::PNGEncoder;
+use image::{ColorType, ImageResult, codecs::png::PngEncoder};
 use std::fs::File;
-
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
+
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    // render(&mut pixels, bounds, upper_left, lower_right);
+
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bound, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        }).unwrap();
+    }
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
+
+
 }
 
 
@@ -104,17 +148,15 @@ fn render(
 }
 
 fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
-               -> Result<(), std::io::Error> {
+               -> ImageResult<()> {
     let output = File::create(filename)?;
 
-    let encoder = PNGEncoder::new(output);
+    let encoder = PngEncoder::new(output);
     encoder.encode(
         pixels,
         bounds.0 as u32, bounds.1 as u32,
-        ColorType::Gray(8)
-    )?;
-
-    Ok(())
+        ColorType::L8
+    )
 }
 
 #[test]
